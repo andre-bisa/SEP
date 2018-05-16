d08b2d5e3fa709810b2232fff3cfc43a6cbe86dc
Andrea Bisacchi <andrea.bisacchi5@studio.unibo.it>
2018-05-16 15:13:24 +0200

    Refactoring Persona
    Mi sono uniformato alle classi create da Giordan. Avevamo anche detto di non fare classi innestate, le avevo fatte

--- IngegneriaDelSoftware/IngegneriaDelSoftware/Model/PersonaFisica.cs
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Model/PersonaFisica.cs
@@ -6,7 +6,74 @@ using System.Threading.Tasks;
 
 namespace IngegneriaDelSoftware.Model
 {
-    class PersonaFisica
+    public class PersonaFisica : Persona
     {
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            private set
+            {
+                _nome = value;
+                base.LanciaEvento();
+            }
+        }
+        private string _cognome;
+        public string Cognome
+        {
+            get
+            {
+                return _cognome;
+            }
+            private set
+            {
+                _cognome = value;
+                base.LanciaEvento();
+            }
+        }
+        private string _partitaIVA;
+        public string PartitaIVA
+        {
+            get
+            {
+                return _partitaIVA;
+            }
+            private set
+            {
+                _partitaIVA = value;
+                base.LanciaEvento();
+            }
+        }
+
+        public override EnumTipoPersona TipoPersona { get { return EnumTipoPersona.Fisica; } }
+
+        #region "Costruttori"
+        public PersonaFisica(string codiceFiscale, string indirizzo, string nome, string cognome) : this(codiceFiscale, indirizzo, nome, cognome, null, null, null)
+        {
+        }
+
+        public PersonaFisica(string codiceFiscale, string indirizzo, string nome, string cognome, List<Telefono> telefoni, List<Email> email) : this(codiceFiscale, indirizzo, nome, cognome, null, telefoni, email)
+        {
+        }
+
+        public PersonaFisica(string codiceFiscale, string indirizzo, string nome, string cognome, string partitaIVA) : this(codiceFiscale, indirizzo, nome, cognome, partitaIVA, null, null)
+        {
+        }
+
+        public PersonaFisica(string codiceFiscale, string indirizzo, string nome, string cognome, string partitaIVA, List<Telefono> telefoni, List<Email> email) : base(codiceFiscale, indirizzo, telefoni, email)
+        {
+            Nome = nome ?? throw new ArgumentNullException(nameof(nome));
+            Cognome = cognome ?? throw new ArgumentNullException(nameof(cognome));
+            PartitaIVA = partitaIVA; // can be null
+        }
+        #endregion
+
+        public override string getDenominazione()
+        {
+            return this.Nome + " " + this.Cognome;
+        }
     }
 }
--- IngegneriaDelSoftware/IngegneriaDelSoftware/Model/PersonaGiuridica.cs
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Model/PersonaGiuridica.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace IngegneriaDelSoftware.Model
 {
-    class PersonaGiuridica
+    public class PersonaGiuridica : Persona
     {
+        private string _ragioneSociale;
+        public string RagioneSociale
+        {
+            get
+            {
+                return _ragioneSociale;
+            }
+            private set
+            {
+                _ragioneSociale = value;
+                base.LanciaEvento();
+            }
+        }
+        private string _sedeLegale;
+        public string SedeLegale
+        {
+            get
+            {
+                return _sedeLegale;
+            }
+            private set
+            {
+                _sedeLegale = value;
+                base.LanciaEvento();
+            }
+        }
+        private string _partitaIVA;
+        public string PartitaIVA
+        {
+            get
+            {
+                return _partitaIVA;
+            }
+            private set
+            {
+                _partitaIVA = value;
+                base.LanciaEvento();
+            }
+        }
+
+        public override EnumTipoPersona TipoPersona { get { return EnumTipoPersona.Giuridica; } }
+
+        #region "Costruttori"
+        public PersonaGiuridica(string codiceFiscale, string indirizzo, string ragioneSociale, string sedeLegale, string partitaIVA) : this(codiceFiscale, indirizzo, ragioneSociale, sedeLegale, partitaIVA, null, null)
+        {
+        }
+
+        public PersonaGiuridica(string codiceFiscale, string indirizzo, string ragioneSociale, string sedeLegale, string partitaIVA, List<Telefono> telefoni, List<Email> email) : base(codiceFiscale, indirizzo, telefoni, email)
+        {
+            RagioneSociale = ragioneSociale ?? throw new ArgumentNullException(nameof(ragioneSociale));
+            SedeLegale = sedeLegale ?? throw new ArgumentNullException(nameof(sedeLegale));
+            PartitaIVA = partitaIVA ?? throw new ArgumentNullException(nameof(partitaIVA));
+        }
+        #endregion
+
+        public override string getDenominazione()
+        {
+            return this.RagioneSociale;
+        }
     }
 }
