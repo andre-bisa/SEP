b9138325d127cb8f5fe6f8bad197ffc5684d64a0
Andrea Bisacchi <andrea.bisacchi5@studio.unibo.it>
2018-05-16 22:37:51 +0200

    Aggiunta FormConfirm
    Form che permette di fare una richiesta all'utente. Concettualmente uguale al MessageBox ma è realizzata in MaterialDesign

---
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/View/FormConfim.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+
+namespace IngegneriaDelSoftware.View
+{
+    public partial class FormConfim : MaterialForm
+    {
+        public string Titolo
+        {
+            get
+            {
+                return this.Text;
+            }
+            set
+            {
+                this.Text = value;
+            }
+        }
+        public string Messaggio
+        {
+            get
+            {
+                return lblMessaggio.Text;
+            }
+            set
+            {
+                lblMessaggio.Text = value;
+            }
+        }
+
+        protected FormConfim()
+        {
+            InitializeComponent();
+        }
+
+        protected FormConfim(string titolo, string messaggio) : this()
+        {
+            Titolo = titolo;
+            Messaggio = messaggio;
+        }
+
+        private void btnAnnulla_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void btnConferma_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        public static DialogResult Show(string titolo, string messaggio)
+        {
+            FormConfim formConfim = new FormConfim(titolo, messaggio);
+            return formConfim.ShowDialog();
+        }
+    }
+}
