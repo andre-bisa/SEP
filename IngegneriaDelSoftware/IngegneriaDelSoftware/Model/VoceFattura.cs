70bd0062e62d01cbca2864d5f64f0649db516234
chkrr00k <chkrr00k@cock.li>
2018-05-17 22:46:36 +0200

    Implementate Fattura, Preventivo, Vendita, Voce, VoceFattura, VocePreventivo, VoceVendita
    -Implementato iterator pattern su fattura, vendita e preventivo
    -Implementato indexer su fattura, vendita e preventivo
    -Implementato ICollection su fattura, vendita e preventivo
    -Implementato IComparable su voce e classi derivate
    
    -Creata piccola funzione di test
    TODO{
    -Migliorare i test
    -Controllare le implementazioni per campi mancanti
    }

---
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Model/VoceFattura.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngegneriaDelSoftware.Model {
+    public class VoceFattura: Voce {
+
+        #region Campi privati
+        private float _iva;
+        #endregion
+
+        #region Proprty
+        /// <summary>
+        /// Il valore dell'iva da calcolare pe la voce
+        /// </summary>
+        public float Iva { get { return this._iva; } }
+        #endregion
+
+        #region Costruttore
+        /// <summary>
+        /// Crea una nuova voce inseribile in qualsiasi <see cref="VoceContainer"/>.
+        /// </summary>
+        /// <param name="causale">La causale della voce.</param>
+        /// <param name="importo">L'importo della voce</param>
+        /// <param name="tipologia">La tipologia della voce. Cioè un campo opzionale 
+        /// riempibile dall'utente per raggruppare le voci in macro gruppi.
+        /// <para>L'importo è sempre da intendersi singolo</para></param>
+        /// <param name="quantita">La quantità di una voce</param>
+        /// <param name="IVA">Il valore dell'iva da calcolare pe la voce</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        public VoceFattura(string causale, decimal importo, float IVA, string tipologia = null, int quantita = 1) : base(causale, importo, tipologia, quantita) {
+            this._iva = IVA;
+        }
+        #endregion
+
+        #region Funzioni pubbliche
+        public override string ToString() {
+            return String.Format("{0}\t{1}", base.ToString(), this._iva);
+        }
+        #endregion
+
+    }
+}
---
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Model/VocePreventivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngegneriaDelSoftware.Model {
+    public class VocePreventivo: Voce {
+
+        #region Costruttore
+        /// <summary>
+        /// Crea una nuova voce inseribile in qualsiasi <see cref="VoceContainer"/>.
+        /// </summary>
+        /// <param name="causale">La causale della voce.</param>
+        /// <param name="importo">L'importo della voce</param>
+        /// <param name="tipologia">La tipologia della voce. Cioè un campo opzionale 
+        /// riempibile dall'utente per raggruppare le voci in macro gruppi.
+        /// <para>L'importo è sempre da intendersi singolo</para></param>
+        /// <param name="quantita">La quantità di una voce</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        public VocePreventivo(string causale, decimal importo, string tipologia = null, int quantita = 1) : base(causale, importo, tipologia, quantita) {
+        }
+        #endregion
+
+    }
+}
