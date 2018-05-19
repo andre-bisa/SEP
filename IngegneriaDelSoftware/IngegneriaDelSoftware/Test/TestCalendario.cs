1ec3456308367caeeac740111ad136b5f9272c5f
Davide Giordano <www.link.1996@hotmail.it>
2018-05-19 18:44:43 +0200

    Aggiunti test al calendario

--- IngegneriaDelSoftware/IngegneriaDelSoftware/Test/TestCalendario.cs
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Test/TestCalendario.cs
@@ -11,6 +11,7 @@ namespace IngegneriaDelSoftware.Test
     [TestFixture]
     public class TestCalendario
     {
+        private Calendario _calendario;
 
         public TestCalendario()
         {
@@ -20,8 +21,8 @@ namespace IngegneriaDelSoftware.Test
             Persona p4 = new PersonaFisica("s", "Via x", "Filippo", "Bianchi");
             DatiAppuntamento datiAppuntamento1 = new DatiAppuntamento(p1, "Riunione", "Bologna", DateTime.Now);
             DatiAppuntamento datiAppuntamento2 = new DatiAppuntamento(p2, "Riunione", "Milano", new DateTime(2018, 4, 30, 12, 0, 0));
-            DatiAppuntamento datiAppuntamento3 = new DatiAppuntamento(p2, "Riunione", "Roma", new DateTime(2018, 4, 30, 12, 0, 0));
-            DatiAppuntamento datiAppuntamento4 = new DatiAppuntamento(p2, "Riunione", "Napoli", new DateTime(2018, 3, 21, 12, 0, 0));
+            DatiAppuntamento datiAppuntamento3 = new DatiAppuntamento(p3, "Riunione", "Roma", new DateTime(2100, 5, 30, 12, 0, 0));
+            DatiAppuntamento datiAppuntamento4 = new DatiAppuntamento(p4, "Riunione", "Napoli", new DateTime(2018, 3, 21, 12, 0, 0));
             Appuntamento a1 = new Appuntamento(datiAppuntamento1);
             Appuntamento a2 = new Appuntamento(datiAppuntamento2);
             Appuntamento a3 = new Appuntamento(datiAppuntamento3);
@@ -29,26 +30,60 @@ namespace IngegneriaDelSoftware.Test
 
 
 
-            Calendario calendario = new Calendario();
+            _calendario = new Calendario();
 
-            calendario.Add(a1);
-            calendario.Add(a2);
-            calendario.Add(a3);
-            calendario.Add(a4);
-
-            List<Appuntamento> appuntamenti = calendario.AppuntamentiDaA(new DateTime(2018, 3, 1, 0, 0, 0), DateTime.Now);
-
-            foreach (Appuntamento a in appuntamenti)
-            {
-                Console.WriteLine(a.ToString());
-            }
+            _calendario.Add(a1);
+            _calendario.Add(a2);
+            _calendario.Add(a3);
+            _calendario.Add(a4);
         }
 
         [Test]
-        public void Test()
+        public void TestRimuovi()
         {
+            Persona p2 = new PersonaFisica("s", "Via x", "Mario", "Verdi");
+            DatiAppuntamento datiAppuntamento2 = new DatiAppuntamento(p2, "Riunione", "Milano", new DateTime(2018, 4, 30, 12, 0, 0));
+            Appuntamento a2 = new Appuntamento(datiAppuntamento2);
 
+            Assert.AreEqual(true, _calendario.Remove(a2));
         }
+
+        [Test]
+        public void TestAggiungi()
+        {
+            Persona p = new PersonaFisica("s", "Via x", "Mario", "Rossi");
+            DatiAppuntamento datiAppuntamento2 = new DatiAppuntamento(p, "Chiacchierare", "Bologna", new DateTime(2018, 4, 30, 12, 0, 0));
+            Appuntamento a2 = new Appuntamento(datiAppuntamento2);
+
+            this._calendario.Add(a2);
+
+            Assert.AreEqual(true, _calendario.Contains(a2));
+        }
+
+        [Test]
+        public void TestDaA()
+        {
+            Persona p1 = new PersonaFisica("s", "Via x", "Mario", "Rossi");
+            Persona p2 = new PersonaFisica("s", "Via x", "Mario", "Verdi");
+            Persona p3 = new PersonaGiuridica("s", "Via x", "REgione Sociale", "Sede Illegale", "2");
+            Persona p4 = new PersonaFisica("s", "Via x", "Filippo", "Bianchi");
+            DatiAppuntamento datiAppuntamento1 = new DatiAppuntamento(p1, "Riunione", "Bologna", DateTime.Now);
+            DatiAppuntamento datiAppuntamento2 = new DatiAppuntamento(p2, "Riunione", "Milano", new DateTime(2018, 4, 30, 12, 0, 0));
+            DatiAppuntamento datiAppuntamento3 = new DatiAppuntamento(p3, "Riunione", "Roma", new DateTime(2100, 5, 30, 12, 0, 0));
+            DatiAppuntamento datiAppuntamento4 = new DatiAppuntamento(p4, "Riunione", "Napoli", new DateTime(2018, 3, 21, 12, 0, 0));
+            Appuntamento a1 = new Appuntamento(datiAppuntamento1);
+            Appuntamento a2 = new Appuntamento(datiAppuntamento2);
+            Appuntamento a3 = new Appuntamento(datiAppuntamento3);
+            Appuntamento a4 = new Appuntamento(datiAppuntamento4);
+
+            List<Appuntamento> appuntamenti = _calendario.AppuntamentiDaA(new DateTime(2018, 3, 1, 0, 0, 0), DateTime.Now);
+
+            Assert.AreEqual(true, appuntamenti.Contains(a1));
+            Assert.AreEqual(false, appuntamenti.Contains(a2));
+            Assert.AreEqual(true, appuntamenti.Contains(a3));
+            Assert.AreEqual(true, appuntamenti.Contains(a4));
+        }
+
 
     }
 }
