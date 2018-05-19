e2a2ad3f07d82076082492ac50cf58ce2cabd55f
Davide Giordano <www.link.1996@hotmail.it>
2018-05-19 18:30:53 +0200

    Test Cakendario modificato

--- IngegneriaDelSoftware/IngegneriaDelSoftware/Test/TestCalendario.cs
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Test/TestCalendario.cs
@@ -1,4 +1,5 @@
 ﻿using IngegneriaDelSoftware.Model;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,24 +8,33 @@ using System.Threading.Tasks;
 
 namespace IngegneriaDelSoftware.Test
 {
+    [TestFixture]
     public class TestCalendario
     {
 
         public TestCalendario()
         {
-            Appuntamento a1 = new Appuntamento("Bologna", new PersonaFisica("s", "Via x", "Mario", "Rossi"), "Riunione", DateTime.Now);
-            Appuntamento a2 = new Appuntamento("Bologna", new PersonaFisica("s", "Via x", "Mario", "Verdi"), "Riunione", new DateTime(2018, 5, 30, 12, 0, 0));
-            Appuntamento a3 = new Appuntamento("Roma", new PersonaFisica("s", "Via x", "Carlo", "Rossi"), "Riunione", new DateTime(2018, 4, 30, 12, 0, 0));
-            Appuntamento a4 = new Appuntamento("Milano", new PersonaFisica("s", "Via x", "Filippo", "Bianchi"), "Riunione", new DateTime(2018, 3, 21, 12, 0, 0));
-            Appuntamento a5 = new Appuntamento("Napoli", new PersonaFisica("s", "Via x", "Fabrizio", "Rossi"), "Riunione", new DateTime(2018, 4, 30, 12, 0, 0));
+            Persona p1 = new PersonaFisica("s", "Via x", "Mario", "Rossi");
+            Persona p2 = new PersonaFisica("s", "Via x", "Mario", "Verdi");
+            Persona p3 = new PersonaGiuridica("s", "Via x", "REgione Sociale", "Sede Illegale", "2");
+            Persona p4 = new PersonaFisica("s", "Via x", "Filippo", "Bianchi");
+            DatiAppuntamento datiAppuntamento1 = new DatiAppuntamento(p1, "Riunione", "Bologna", DateTime.Now);
+            DatiAppuntamento datiAppuntamento2 = new DatiAppuntamento(p2, "Riunione", "Milano", new DateTime(2018, 4, 30, 12, 0, 0));
+            DatiAppuntamento datiAppuntamento3 = new DatiAppuntamento(p2, "Riunione", "Roma", new DateTime(2018, 4, 30, 12, 0, 0));
+            DatiAppuntamento datiAppuntamento4 = new DatiAppuntamento(p2, "Riunione", "Napoli", new DateTime(2018, 3, 21, 12, 0, 0));
+            Appuntamento a1 = new Appuntamento(datiAppuntamento1);
+            Appuntamento a2 = new Appuntamento(datiAppuntamento2);
+            Appuntamento a3 = new Appuntamento(datiAppuntamento3);
+            Appuntamento a4 = new Appuntamento(datiAppuntamento4);
+
+
 
             Calendario calendario = new Calendario();
 
-            calendario.AggiungiAppuntamento(a1);
-            calendario.AggiungiAppuntamento(a2);
-            calendario.AggiungiAppuntamento(a3);
-            calendario.AggiungiAppuntamento(a4);
-            calendario.AggiungiAppuntamento(a5);
+            calendario.Add(a1);
+            calendario.Add(a2);
+            calendario.Add(a3);
+            calendario.Add(a4);
 
             List<Appuntamento> appuntamenti = calendario.AppuntamentiDaA(new DateTime(2018, 3, 1, 0, 0, 0), DateTime.Now);
 
@@ -32,6 +42,12 @@ namespace IngegneriaDelSoftware.Test
             {
                 Console.WriteLine(a.ToString());
             }
+        }
+
+        [Test]
+        public void Test()
+        {
+
         }
 
     }
