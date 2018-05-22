b988611f63e5899a14840dd84eb0caf98b272b78
Andrea Bisacchi <andrea.bisacchi5@studio.unibo.it>
2018-05-22 16:50:06 +0200

    Aggiunto HashCode ad Appuntamento

--- IngegneriaDelSoftware/IngegneriaDelSoftware/Model/Appuntamento.cs
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Model/Appuntamento.cs
@@ -62,15 +62,25 @@ namespace IngegneriaDelSoftware.Model
                    DataOra == appuntamento.DataOra;
         }
 
+        public override int GetHashCode()
+        {
+            var hashCode = -1334492624;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Oggetto);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Luogo);
+            hashCode = hashCode * -1521134295 + DataOra.GetHashCode();
+            return hashCode;
+        }
+
         public static bool operator ==(Appuntamento appuntamento1, Appuntamento appuntamento2)
         {
-            return EqualityComparer<Appuntamento>.Default.Equals(appuntamento1, appuntamento2);
+            return appuntamento1.Equals(appuntamento2);
         }
 
         public static bool operator !=(Appuntamento appuntamento1, Appuntamento appuntamento2)
         {
             return !(appuntamento1 == appuntamento2);
         }
+
         #endregion
 
     }
