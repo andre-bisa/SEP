f9a204d7437a353aa764cfd22355ad1408e77d7e
Andrea Bisacchi <andrea.bisacchi5@studio.unibo.it>
2018-05-23 17:31:46 +0200

    Classe di Model che memorizza una mail inviata, utile per lo storico delle MailInviate, appunto

---
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Model/CollezioneEmailInviate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngegneriaDelSoftware.Model
+{
+    public class CollezioneEmailInviate : IEnumerable<MailInviata>, ICollection<MailInviata>
+    {
+        private List<MailInviata> _email = new List<MailInviata>();
+
+        protected CollezioneEmailInviate()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                this._email.Add(new MailInviata(i, new DateTime(2018, 5, i+1), "Oggetto" + i, "Corpo", "mail@mail.com"));
+            }
+        }
+
+        #region Singleton
+        private static CollezioneEmailInviate _listaEmailInviate = null;
+        public static CollezioneEmailInviate GetInstance()
+        {
+            if (_listaEmailInviate == null)
+                _listaEmailInviate = new CollezioneEmailInviate();
+            return _listaEmailInviate;
+        }
+        #endregion
+
+        public int Count => ((ICollection<MailInviata>)_email).Count;
+
+        public bool IsReadOnly => ((ICollection<MailInviata>)_email).IsReadOnly;
+
+        public IEnumerator<MailInviata> GetEnumerator()
+        {
+            return ((IEnumerable<MailInviata>)_email).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<MailInviata>)_email).GetEnumerator();
+        }
+
+        public void Add(MailInviata item)
+        {
+            ((ICollection<MailInviata>)_email).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((ICollection<MailInviata>)_email).Clear();
+        }
+
+        public bool Contains(MailInviata item)
+        {
+            return ((ICollection<MailInviata>)_email).Contains(item);
+        }
+
+        public void CopyTo(MailInviata[] array, int arrayIndex)
+        {
+            ((ICollection<MailInviata>)_email).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(MailInviata item)
+        {
+            return ((ICollection<MailInviata>)_email).Remove(item);
+        }
+
+        public override string ToString()
+        {
+            return String.Join(",", this._email);
+        }
+
+    }
+}
---
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Model/MailInviata.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngegneriaDelSoftware.Model
+{
+    public class MailInviata
+    {
+        public int ID { get; private set; }
+        public DateTime Data { get; private set; }
+        public string Oggetto { get; private set; }
+        public string Corpo { get; private set; }
+        public string Email { get; private set; }
+
+        public MailInviata(int iD, DateTime data, string oggetto, string corpo, string email)
+        {
+            ID = iD;
+            Data = data;
+            Oggetto = oggetto;
+            Corpo = corpo;
+            Email = email;
+        }
+
+        #region Equals
+        public override bool Equals(object obj)
+        {
+            var inviata = obj as MailInviata;
+            return inviata != null &&
+                   ID == inviata.ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1213502048 + ID.GetHashCode();
+        }
+
+        public static bool operator ==(MailInviata inviata1, MailInviata inviata2)
+        {
+            return EqualityComparer<MailInviata>.Default.Equals(inviata1, inviata2);
+        }
+
+        public static bool operator !=(MailInviata inviata1, MailInviata inviata2)
+        {
+            return !(inviata1 == inviata2);
+        }
+        #endregion
+    }
+}
