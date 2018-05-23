9cdc5b7d85bf16add3b435dcdab959d1ebc5334f
chkrr00k <chkrr00k@cock.li>
2018-05-23 22:28:18 +0200

    Mimic delle classi MySQL
    TODO{
    - implementazione
    }

---
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Persistenza/XML/XMLDaoFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IngegneriaDelSoftware.Persistenza.Dao;
+
+namespace IngegneriaDelSoftware.Persistenza.XML {
+    class XMLDaoFactory: PersistenzaFactory {
+
+        private string _clienteXMLFile;
+        private string _personaXMLFile;
+
+        public string ClienteXMLFile {
+            get {
+                return _clienteXMLFile;
+            }
+        }
+
+        public string PersonaXMLFile {
+            get {
+                return _personaXMLFile;
+            }
+        }
+
+        protected XMLDaoFactory(string clienteXMLFile, string personaXMLFile) {
+            this._clienteXMLFile = clienteXMLFile;
+            this._personaXMLFile = personaXMLFile;
+        }
+
+        #region Singleton
+        private static XMLDaoFactory _instance = null;
+
+        //TODO fix this;
+        public static XMLDaoFactory GetInstance(string clienteXMLFile = null, string personaXMLFile = null) {
+            if(_instance == null)
+                if(clienteXMLFile == null && personaXMLFile == null) {
+                    throw new ArgumentNullException("L'inizializzazione deve avere gli attributi diversi da null");
+                }
+                _instance = new XMLDaoFactory(clienteXMLFile, personaXMLFile);
+            return _instance;
+        }
+        #endregion
+        
+
+        public override IClienteDAO GetClienteDAO() {
+            return new XMLClienteDao(this._clienteXMLFile);
+        }
+
+        public override IPersonaDAO GetPersonaDAO() {
+            return new XMLPersonaDao(this._personaXMLFile);
+        }
+    }
+}
---
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Persistenza/XML/XMLPersonaDao.cs
@@ -0,0 +1,41 @@
+﻿using IngegneriaDelSoftware.Persistenza.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IngegneriaDelSoftware.Model;
+
+namespace IngegneriaDelSoftware.Persistenza.XML {
+    class XMLPersonaDao: IPersonaDAO {
+        private string _personaXMLFile;
+
+        public XMLPersonaDao(string personaXMLFile) {
+            this._personaXMLFile = personaXMLFile;
+        }
+
+        public bool Aggiorna(Persona personaVecchia, DatiPersona datiPersona) {
+            throw new NotImplementedException();
+        }
+
+        public bool Aggiorna(Persona personaVecchia, DatiPersona datiPersona, string ID) {
+            throw new NotImplementedException();
+        }
+
+        public void Crea(DatiPersona persona) {
+            throw new NotImplementedException();
+        }
+
+        public void Crea(DatiPersona persona, string ID) {
+            throw new NotImplementedException();
+        }
+
+        public bool Elimina(string CF) {
+            throw new NotImplementedException();
+        }
+
+        public Persona Leggi(string CF) {
+            throw new NotImplementedException();
+        }
+    }
+}
