8d8536d271e40369123d0fb7f11723284793411f
chkrr00k <chkrr00k@localhost.localdomain>
2018-05-25 12:00:40 +0200

    Args fattura vendita e preventivo

---
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Model/ArgsEvent/ArgsFattura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngegneriaDelSoftware.Model.ArgsEvent
+{
+    public class ArgsFattura : EventArgs
+    {
+        public Fattura Fattura { get; private set; }
+
+        public ArgsFattura(Fattura fattura)
+        {
+            this.Fattura = fattura;
+        }
+
+    }
+}
---
+++ IngegneriaDelSoftware/IngegneriaDelSoftware/Model/ArgsEvent/ArgsPreventivi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngegneriaDelSoftware.Model.ArgsEvent
+{
+    public class ArgsPreventivo : EventArgs
+    {
+        public Preventivo Preventivo { get; private set; }
+
+        public ArgsPreventivo(Preventivo preventivo)
+        {
+            this.Preventivo = preventivo;
+        }
+
+    }
+}
