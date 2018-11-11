7fe06dc08d16c8e2b481733118fbcc055263853a
Andrea Bisacchi <andrea.bisacchi5@studio.unibo.it>
2018-11-11 17:34:35 +0100

    MaterialLabel ora ha le animazioni
    Ho fatto sì che anche il MaterialLabel avesse le animazioni (entrata del mouse e click). Si possono abilitare dalla opportuna proprietà dirattemente dalla casella delle proprietà in fase di progettazione della grafica. Molto utile per inserire un'icona cliccabile

--- MaterialSkin/MaterialSkin/Controls/MaterialLabel.cs
+++ MaterialSkin/MaterialSkin/Controls/MaterialLabel.cs
@@ -1,4 +1,8 @@
-﻿using System.ComponentModel;
+﻿using MaterialSkin.Animations;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Windows.Forms;
 
 namespace MaterialSkin.Controls
@@ -11,6 +15,30 @@ namespace MaterialSkin.Controls
         public MaterialSkinManager SkinManager => MaterialSkinManager.Instance;
         [Browsable(false)]
         public MouseState MouseState { get; set; }
+
+        public bool EnableAnimations { get; set; } = false;
+
+        private readonly AnimationManager _hoverAnimationManager;
+        private readonly AnimationManager _animationManager;
+
+        public MaterialLabel()
+        {
+            _animationManager = new AnimationManager(false)
+            {
+                Increment = 0.03,
+                AnimationType = AnimationType.EaseOut
+            };
+
+            _hoverAnimationManager = new AnimationManager
+            {
+                Increment = 0.07,
+                AnimationType = AnimationType.Linear
+            };
+
+            _hoverAnimationManager.OnAnimationProgress += sender => Invalidate();
+            _animationManager.OnAnimationProgress += sender => Invalidate();
+        }
+
         protected override void OnCreateControl()
         {
             base.OnCreateControl();
@@ -19,6 +47,81 @@ namespace MaterialSkin.Controls
             Font = SkinManager.ROBOTO_REGULAR_11;
 
             BackColorChanged += (sender, args) => ForeColor = SkinManager.GetPrimaryTextColor();
+
+            if (DesignMode) return;
+            if (!EnableAnimations) return;
+
+            MouseState = MouseState.OUT;
+            MouseEnter += (sender, args) =>
+            {
+                MouseState = MouseState.HOVER;
+                _hoverAnimationManager.StartNewAnimation(AnimationDirection.In);
+                Invalidate();
+            };
+            MouseLeave += (sender, args) =>
+            {
+                MouseState = MouseState.OUT;
+                _hoverAnimationManager.StartNewAnimation(AnimationDirection.Out);
+                Invalidate();
+            };
+            MouseDown += (sender, args) =>
+            {
+                if (args.Button == MouseButtons.Left)
+                {
+                    MouseState = MouseState.DOWN;
+
+                    _animationManager.StartNewAnimation(AnimationDirection.In, args.Location);
+                    Invalidate();
+                }
+            };
+            MouseUp += (sender, args) =>
+            {
+                MouseState = MouseState.HOVER;
+
+                Invalidate();
+            };
         }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+
+            if (!EnableAnimations) return;
+
+            var g = pevent.Graphics;
+            g.TextRenderingHint = TextRenderingHint.AntiAlias;
+
+            //Hover
+            Color c = SkinManager.GetFlatButtonHoverBackgroundColor(); // FIXME change color
+            using (Brush b = new SolidBrush(Color.FromArgb((int)(_hoverAnimationManager.GetProgress() * c.A), c.RemoveAlpha())))
+                g.FillRectangle(b, ClientRectangle);
+
+            //Ripple
+            if (_animationManager.IsAnimating())
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                for (var i = 0; i < _animationManager.GetAnimationCount(); i++)
+                {
+                    var animationValue = _animationManager.GetProgress(i);
+                    var animationSource = _animationManager.GetSource(i);
+
+                    using (Brush rippleBrush = new SolidBrush(Color.FromArgb((int)(101 - (animationValue * 100)), Color.Black)))
+                    {
+                        var rippleSize = (int)(animationValue * Width * 2);
+                        g.FillEllipse(rippleBrush, new Rectangle(animationSource.X - rippleSize / 2, animationSource.Y - rippleSize / 2, rippleSize, rippleSize));
+                    }
+                }
+                g.SmoothingMode = SmoothingMode.None;
+            }
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.ResumeLayout(false);
+
+        }
+
     }
 }
